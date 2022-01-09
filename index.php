d3641a32dc93d2270b79fbc80b12adb3a9a1bc83
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2022-01-09 22:52:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 
 
-$botToken = "5095328253:AAGDz3vfHSt06Y6gBu9AbGHg7g0bENz0rQ8"; // Enter ur bot token
+$botToken = "5093083591:AAE59zqIX3v0kocOoRp4g70UsEHeM6F96LM"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
